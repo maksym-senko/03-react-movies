7b498c59712f0b2ed6c77737d472db79567bb09b
Maksym <maksskazka1997@gmail.com>
2026-03-13 18:25:05 +0100

    correct token

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -3,18 +3,24 @@ import type { Movie } from '../types/movie';
 
 interface MovieResponse {
   results: Movie[];
+  page: number;
+  total_results: number;
+  total_pages: number;
 }
 
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
   const response = await axios.get<MovieResponse>(`${BASE_URL}/search/movie`, {
     params: {
-      api_key: API_KEY,
       query: query,
       language: 'uk-UA',
       include_adult: false,
+    },
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+      accept: 'application/json',
     },
   });
 
